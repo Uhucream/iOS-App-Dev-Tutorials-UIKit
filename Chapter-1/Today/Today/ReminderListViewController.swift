a0e3d205dfe4af8f001acaa774d066c14213dc02
Uhucream <aborog1324@yahoo.co.jp>
2023-06-02 12:23:21 +0900

    [Section 5] Configure the data source

--- Chapter-1/Today/Today/ReminderListViewController.swift
+++ Chapter-1/Today/Today/ReminderListViewController.swift
@@ -1,14 +1,18 @@
 //
 //  ReminderListViewController.swift
-//  
-//  
+//
+//
 //  Created by TakashiUshikoshi on 2023/06/02
-//  
+//
 //
 
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    
+    var dataSource: DataSource!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +20,26 @@ class ReminderListViewController: UICollectionViewController {
         let listLayout = listLayout()
         
         self.collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration: UICollectionView.CellRegistration = .init {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder: Reminder = .sampleData[indexPath.item]
+            
+            var contentConfiguration: UIListContentConfiguration = cell.defaultContentConfiguration()
+            
+            contentConfiguration.text = reminder.title
+            
+            cell.contentConfiguration = contentConfiguration
+        }
+        
+        dataSource = DataSource(collectionView: self.collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
+        }
     }
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
